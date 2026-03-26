42b80697b7d2c9b5487e84740f80639c6781700b
jade <jade.paypa@hotwork.asia>
2026-03-26 16:45:59 +0800

    new hero section and fixing section height

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,75 +1,98 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-      <main className="flex-1 px-6 h-[calc(100vh-64px)]">
+      <header className="bg-white shadow-md py-2 px-6 fixed h-[95px] w-full top-0 z-100">
+        <div className="flex justify-between items-center max-w-7xl mx-auto">
+          <Image
+            src="/logo/lb-apparel-logo.jpg"
+            alt="LB Apparel Logo"
+            width={80}
+            height={50}
+            className="object-contain"
+          />
+          <div className="flex gap-5 items-center">
+            <nav className="hidden md:flex space-x-8">
+              <Link
+                href="/"
+                className="text-gray-700 hover:text-gray-900 font-medium"
+              >
+                Home
+              </Link>
+              <Link
+                href="/collections"
+                className="text-gray-700 hover:text-gray-900 font-medium"
+              >
+                Collections
+              </Link>
+              <Link
+                href="/community"
+                className="text-gray-700 hover:text-gray-900 font-medium"
+              >
+                Community
+              </Link>
+              <Link
+                href="/about"
+                className="text-gray-700 hover:text-gray-900 font-medium"
+              >
+                About Us
+              </Link>
+            </nav>
+            <div className="flex items-center space-x-4">
+              <button className="text-gray-700 hover:text-gray-900">
+                <svg
+                  className="w-6 h-6"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+        </div>
+      </header>
+      <main className="flex-1 h-[calc(100vh-64px)] bg-white">
         <section
           aria-label="hero-section"
-          className="snap-center h-[calc(100vh-64px)] flex pt-4"
+          className="snap-center h-screen pt-[95px] flex bg-linear-to-b from-gray-300 via-gray-500 to-gray-700"
         >
-          <div className="max-w-7xl mx-auto">
-            <div className="w-full">
-              <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                <div className="flex flex-col gap-4">
-                  <div className="flex justify-between gap-4">
-                    <div className="overflow-hidden rounded-lg flex-1">
-                      <Image
-                        src="/hero/hero1.jpg"
-                        alt="Hero 1"
-                        width={800}
-                        height={540}
-                        className="w-full h-80 md:h-97 object-cover"
-                      />
-                    </div>
-                    <div className="overflow-hidden rounded-lg flex-1">
-                      <Image
-                        src="/hero/hero2.jpg"
-                        alt="Hero 2"
-                        width={800}
-                        height={540}
-                        className="w-full h-80 md:h-97 object-cover"
-                      />
-                    </div>
-                  </div>
-                  <div className="flex justify-between gap-4">
-                    <div className="overflow-hidden rounded-lg flex-1">
-                      <Image
-                        src="/hero/hero3.jpg"
-                        alt="Hero 3"
-                        width={800}
-                        height={540}
-                        className="w-full h-80 md:h-97 object-cover"
-                      />
-                    </div>
-                    <div className="overflow-hidden rounded-lg flex-1">
-                      <Image
-                        src="/hero/hero4.jpg"
-                        alt="Hero 4"
-                        width={800}
-                        height={540}
-                        className="w-full h-80 md:h-97 object-cover"
-                      />
-                    </div>
-                  </div>
-                </div>
-                <div className="overflow-hidden rounded-lg">
-                  <Image
-                    src="/hero/hero4.jpg"
-                    alt="Hero 4"
-                    width={800}
-                    height={540}
-                    className="w-full h-full object-cover"
-                  />
-                </div>
-              </div>
+          <div className="relative flex justify-center w-full mx-auto">
+            <div className="flex flex-col justify-center pt-40 items-start gap-5 text-white">
+              <span className="flex flex-col">
+                <p className="font-bold text-[4rem]">LB Apparel</p>
+                <p className="text-base">“Discover timeless pieces designed for comfort, <br /> style, and confidence every day”</p>
+              </span>
+              <button className="bg-gray-800 cursor-pointer px-5 py-3 rounded-full text-white border-transparent text-base">SHOP NOW  🡢</button>
             </div>
+            <div className="absolute">
+              <p
+                className="font-semibold text-[16rem] tracking-[15px] text-shadow-lg text-gray-600"
+              >
+                SHEESH
+              </p>
+            </div>
+            <Image
+              src="/hero/hero41.png"
+              alt="Hero 4"
+              width={800}
+              height={540}
+              className="w-auto h-auto object-cover z-10"
+            />
           </div>
         </section>
 
         <section
           aria-label="Collection carousel"
-          className="snap-center h-[calc(100vh-64px)] flex items-center py-10"
+          className="snap-center py-30 flex items-center bg-white"
         >
           <div className="max-w-7xl mx-auto">
             <div className="w-full">
@@ -126,7 +149,7 @@ export default function Home() {
 
         <section
           aria-label="Customer reviews"
-          className="snap-center h-[calc(100vh-64px)] flex bg-gray-50 px-5 py-10 items-center"
+          className="snap-center flex bg-gray-300 px-5 py-60 items-center"
         >
           <div className="max-w-7xl mx-auto">
             <div className="flex flex-col gap-20 w-full ">
@@ -170,7 +193,7 @@ export default function Home() {
                       key={review.name}
                       className="rounded-lg border border-gray-200 bg-white p-4 shadow-sm"
                     >
-                      <p className="text-gray-700">"{review.text}"</p>
+                      <p className="text-gray-700">{review.text}</p>
                       <p className="mt-3 font-semibold text-gray-900">
                         - {review.name}
                       </p>
@@ -179,7 +202,7 @@ export default function Home() {
                 </div>
               </div>
               <div className="w-full flex justify-center px-28">
-                <p className="italic text-center">
+                <p className="italic text-center text-black">
                   Our customers trust us for quality, comfort, and style—here’s
                   what they have to say about their experience with our brand.
                   Built for everyday wear and designed to stand out, our pieces
@@ -193,7 +216,7 @@ export default function Home() {
 
         <section
           aria-label="About us"
-          className="snap-center h-[calc(100vh-64px)] flex items-center py-10"
+          className="snap-center h-[calc(100vh-64px)] flex items-center py-10 bg-white"
         >
           <div className="max-w-7xl mx-auto">
             <div className="w-full">
@@ -212,7 +235,7 @@ export default function Home() {
                   <p className="mt-3 text-gray-700">
                     Founded with the mission to make premium apparel accessible,
                     our team combines innovation, design, and conscious sourcing
-                    to deliver products you'll love.
+                    to deliver products you&apos;ll love.
                   </p>
                   <ul className="mt-4 list-disc space-y-1 pl-5 text-gray-600">
                     <li>Quality materials and careful craftsmanship</li>
